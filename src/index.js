f11a7b033932b4b660d3db5c254d83dfa6ad03e8
Artem Surkov <artsur@mail.ru>
2021-03-15 01:27:14 +0300

    Use react + redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {compose, createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {rootReducer} from './redux/rootReduser';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+render(
+  app,
   document.getElementById('root')
 );
---
+++ src/redux/actions.js
@@ -0,0 +1,19 @@
+import {CREATE_POST, FETCH_POSTS} from './types';
+
+export function createPost(post) {
+    return {
+        type: CREATE_POST,
+        payload: post
+    }
+}
+
+export function fetchPosts() {
+    return async dispatch => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1');
+        const json = await response.json();
+        dispatch({
+            type: FETCH_POSTS,
+            payload: json
+        });
+    }
+}
