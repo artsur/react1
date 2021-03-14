51b2cc11ee4198c5539945db66ecfc8968a7d591
Artem Surkov <artsur@mail.ru>
2021-03-15 02:04:17 +0300

    Loader

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -7,8 +7,18 @@ import {fetchPosts} from '../redux/actions';
 const FetchedPosts = ()=> {
     const dispatch = useDispatch();
     const posts = useSelector(state => state.posts.fetchedPosts );
+    const loading = useSelector( state => state.app.loading);
 
     const postsList = posts.map(el=>(<Post key={el.id} post={el}/>));
+
+    const loadingIcon = (
+        <div className='text-center py-3'>
+            <div className={'spinner-grow spinner-grow-sm text-primary mx-3'}/>
+            <div className={'spinner-grow spinner-grow-sm text-primary mx-3'}/>
+            <div className={'spinner-grow spinner-grow-sm text-primary mx-3'}/>
+        </div>
+    )
+
 
     const loadBtn = (
         <button
@@ -23,7 +33,7 @@ const FetchedPosts = ()=> {
         <div className='card shadow'>
             <div className='card-header'><h2>FETCHED POSTS</h2></div>
             <div className='card-body'>
-                {postsList.length ? postsList : loadBtn}
+                {loading ? loadingIcon : ( postsList.length ? postsList : loadBtn ) }
             </div>
         </div>
     );
--- src/redux/rootReduser.js
+++ src/redux/rootReduser.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import {postsReducer} from './postsReduser';
+import {appReduser} from './appReduser';
 
 export const rootReducer = combineReducers({
-    posts: postsReducer
+    posts: postsReducer,
+    app: appReduser
 })
